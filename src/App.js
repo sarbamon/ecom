acd4c17bb0eac16cc4002a34cbb71e8106296446
sarbamon ronghang <sarbamonronghang934@gmail.com>
2024-09-05 10:28:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="text-center">
+      <h1 className="text-red-700 text-5xl underline hover:decoration-dashed p-12 drop-shadow-lg">Hello World</h1>
     </div>
+    <main className="text-green-600 text-center text-xl">
+     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos praesentium nobis dolore neque repudiandae rerum dignissimos necessitatibus harum earum. Natus excepturi laboriosam sed quisquam quae at officia! Alias, placeat tempora!
+    </main>
+    </>
   );
 }
 
